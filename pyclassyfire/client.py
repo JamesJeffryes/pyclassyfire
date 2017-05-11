618c11f918c8b33ecba6adcd4ad06917be125062
James Jeffryes <jamesgjeffryes@gmail.com>
2017-05-11 11:22:01 -0500

    implementation of basic queries

--- pyclassyfire/client.py
+++ pyclassyfire/client.py
@@ -2,36 +2,45 @@
 chemical database files"""
 
 import requests
+import csv
 
 url = "http://classyfire.wishartlab.com"
 
 
-def structure_query(label, input):
+def structure_query(compound, label='pyclassyfire'):
     """Submit a compound information to the ClassyFire service for evaluation 
     and receive a id which can be used to used to collect results
     
-    :param label: 
-    :type label: str
-    :param input: The compound structures as a tab-separated list of id and 
-    inchikey or smiles
-    :type input: str
-    :return: query_id
-    :rtype: str
+    :param compound: The compound structures as line delimited inchikey or smiles.
+    Optionally a tab-separated id may be prepended for each structure.
+    
+    :type compound: str
+    :return: A query ID number
+    :rtype: int
     """
-    raise NotImplementedError
+    r = requests.post(url + '/queries.json', data='{"label": "%s", '
+                      '"query_input": "%s", "query_type": "STRUCTURE"}'
+                                                  % (label, compound),
+                      headers={"Content-Type": "application/json"})
+    r.raise_for_status()
+    return r.json()['id']
 
 
-def get_results(query_id, format="json"):
+def get_results(query_id, return_format="json"):
     """Given a query_id, fetch the classification results.
     
     :param query_id: A numeric query id returned at time of query submission
     :type query_id: str
-    :param format: desired return format. valid types are json, csv or sdf
-    :type format: str
+    :param return_format: desired return format. valid types are json, csv or sdf
+    :type return_format: str
     :return: query information
     :rtype: str
     """
-    raise NotImplementedError
+    r = requests.get('%s/queries/%s.%s' % (url, query_id, return_format),
+                     headers={"Content-Type": "application/%s" % return_format})
+    r.raise_for_status()
+    print(repr(r.text))
+    return r.text
 
 
 def tabular_query(inpath, structure_key, dialect='excel', outpath=None):
